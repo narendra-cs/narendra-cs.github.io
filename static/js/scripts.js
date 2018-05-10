c66c2b4915f96622c25bc7f8bad3804a068800f9
narendra singh <narendra.cs.biet@gmail.com>
2018-05-11 04:29:30 +0530

    Home page created

---
+++ static/js/scripts.js
@@ -0,0 +1,61 @@
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        var prealoaderOption = $(window);
+        prealoaderOption.on("load", function () {
+            var preloader = jQuery('.preloader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(350).fadeOut('slow');
+        });
+
+        var headertopoption = $(window);
+        var headTop = $('.header-top-area');
+
+        headertopoption.on('scroll', function () {
+            if (headertopoption.scrollTop() > 200) {
+                headTop.addClass('menu-bg');
+            } else {
+                headTop.removeClass('menu-bg');
+            }
+        });
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+
+        var parallaxeffect = $(window);
+        parallaxeffect.stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        new WOW().init();
+
+    });
+
+})(jQuery);
