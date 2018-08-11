d04f0603ed825f73b7b95f8eab198d1acc046aee
narendra singh <narendra.cs.biet@gmail.com>
2018-08-11 16:48:57 +0530

    smoth-scroll working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,29 +58,29 @@
         });
 
 
-// direct browser to top right away
-if (window.location.hash)
-    scroll(0,0);
-// takes care of some browsers issue
-setTimeout(function(){scroll(0,0);},1);
+        // direct browser to top right away
+        if (window.location.hash)
+            scroll(0,0);
+        // takes care of some browsers issue
+        setTimeout(function(){scroll(0,0);},1);
 
-$(function(){
-//your current click function
-$('a.smooth-scroll').on('click',function(e){
-    e.preventDefault();
-    $('html,body').animate({
-        scrollTop:$($(this).attr('href')).offset().top - 50
-    },1000,'swing');
-});
+        $(function(){
+        //your current click function
+        $('a.smooth-scroll').on('click',function(e){
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop:$($(this).attr('href')).offset().top - 50
+            },1000,'swing');
+        });
 
-// if we have anchor on the url (calling from other page)
-if(window.location.hash){
-    // smooth scroll to the anchor id
-    $('html,body').animate({
-        scrollTop:$(window.location.hash).offset().top - 50
-        },1000,'swing');
-    }
-});
+        // if we have anchor on the url (calling from other page)
+        if(window.location.hash){
+            // smooth scroll to the anchor id
+            $('html,body').animate({
+                scrollTop:$(window.location.hash).offset().top - 50
+                },1000,'swing');
+            }
+        });
 
               /*
          * ----------------------------------------------------------------------------------------
